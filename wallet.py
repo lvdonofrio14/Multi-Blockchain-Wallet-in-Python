ac08ed51088f18ac68cfe74d658c9c8c72f2062e
lvdonofrio14 <78940231+lvdonofrio14@users.noreply.github.com>
2021-07-12 14:46:24 -0400

    Add files via upload

--- wallet.py
+++ wallet.py
@@ -2,34 +2,56 @@
 import subprocess
 import json
 from dotenv import load_dotenv
+import os 
+from pprint import pprint 
+from web3 import Account, middleware, Web3
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3.middleware import geth_poa_middleware
 
 # Load and set environment variables
 load_dotenv()
-mnemonic=os.getenv("mnemonic")
+mnemonic= os.getenv("mnemonic")
 
 # Import constants.py and necessary functions from bit and web3
-# YOUR CODE HERE
- 
- 
-# Create a function called `derive_wallets`
-def derive_wallets(# YOUR CODE HERE):
-    command = # YOUR CODE HERE
+from constants import *
+
+ # Create a function called `derive_wallets`
+def derive_wallets(coin=BTC, mnemonic=mnemonic, depth=3):
+     #./derive --key=xprv9zbB6Xchu2zRkf6jSEnH9vuy7tpBuq2njDRr9efSGBXSYr1QtN8QHRur28QLQvKRqFThCxopdS1UD61a5q6jGyuJPGLDV9XfYHQto72DAE8 --cols=path,address --coin=ZEC --numderive=3 -g
+    command =f'php ./derive -g --mnemonic="{mnemonic}" --cols=all --coin={coin} --numderive={depth} --format=json'
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     p_status = p.wait()
     return json.loads(output)
 
 # Create a dictionary object called coins to store the output from `derive_wallets`.
-coins = # YOUR CODE HERE
+coins = {
+    ETH: derive_wallets(coin=ETH),
+    BTCTEST: derive_wallets(coin=BTCTEST)
+}
 
 # Create a function called `priv_key_to_account` that converts privkey strings to account objects.
-def priv_key_to_account(# YOUR CODE HERE):
-    # YOUR CODE HERE
-
+def priv_key_to_account(coin, priv_key):
+    if coin == 'BTCTEST':
+        return PrivateKeyTestnet(priv_key)
+    elif coin == 'ETH':
+        return Account.privateKeyToAccount(priv_key)
+    
 # Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
-def create_tx(# YOUR CODE HERE):
-    # YOUR CODE HERE
-
+def create_tx(coin, account, to, amount):
+    if coin == ETH:
+        gasEstimate = w3.eth.estimateGas({"from": account.address, "to": to, "value": amount})
+        return{"from": account.address, "to": to, "value": amount, "gasPrice": w3.eth.gasPrice, "gas": gasEstimate, "nonce": w3.eth.getTransactionCount(account.address), "chainID": w3.eth.chain_id}
+    if coin == BTCTEST:
+        return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
+    
 # Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
-def send_tx(# YOUR CODE HERE):
-    # YOUR CODE HERE
+def send_tx(coin, account, to, amount):
+    if coin == ETH:
+        raw_tx = create_tx(coin, account, to, amount)
+        signed = account.signTransaction(raw_tx)
+        return w3.eth.sendRawTransaction(signed.rawTransaction)
+    if coin == BTCTEST:
+        raw_tx = create_tx(coin, account, to, amount)
+        signed = account.sign_transaction(raw_tx)
+        return NetworkAPI.broadcast_tx_testnet(signed)
